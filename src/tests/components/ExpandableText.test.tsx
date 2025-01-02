1d13d1b7470700d917efd202da9f221ba6dc3e7f
DemaPy <benderdema@gmail.com>
2025-01-02 16:35:13 +0100

    Test cases for Expandable Text created

--- src/tests/components/ExpandableText.test.tsx
+++ src/tests/components/ExpandableText.test.tsx
@@ -1,17 +1,45 @@
 import { render, screen } from "@testing-library/react";
 import ExpandableText from "../../components/ExpandableText";
+import userEvent from "@testing-library/user-event";
 
-describe("Greet", () => {
+describe("Expandable text", () => {
   it("Should render article with short text", () => {
     render(<ExpandableText text="test case" />);
+    const article = screen.getByRole("article");
+    expect(article).toBeInTheDocument();
+    expect(article).toHaveTextContent(/test case/i);
   });
-  it("Should render button with Show Less when clicked", () => {
-    render(<ExpandableText text="test case" />);
+  it("Should render 255 symbols", () => {
+    const testText = "a".repeat(255);
+    render(<ExpandableText text={testText} />);
+    expect(screen.getByText(testText)).toBeInTheDocument();
   });
-  it("Should render button with Show More when clicked", () => {
-    render(<ExpandableText text="test case" />);
+  it("Should render 256 symbols with dots and Show More button", () => {
+    const testText = "a".repeat(256);
+    render(<ExpandableText text={testText} />);
+    const truncated = testText.slice(0, 255) + "..."
+    expect(screen.getByText(truncated)).toBeInTheDocument()
+    expect(screen.getByText("Show More")).toBeInTheDocument();
   });
-  it("Should render 255 symbols when not expanded", () => {
-    render(<ExpandableText text="test case" />);
+  it("Should change text content for button when clicked", async () => {
+    const testText = "a".repeat(256);
+    render(<ExpandableText text={testText} />);
+    const user = userEvent.setup()
+    await user.click(screen.getByText("Show More"))
+
+    expect(screen.getByText(testText)).toBeInTheDocument();
+    expect(screen.getByText("Show Less")).toHaveTextContent("Show Less");
+  });
+  it("Should change text content for button when clicked", async () => {
+    const testText = "a".repeat(256);
+    const truncated = testText.slice(0, 255) + "..."
+
+    render(<ExpandableText text={testText} />);
+    const user = userEvent.setup()
+    await user.click(screen.getByText("Show More"))
+    await user.click(screen.getByText("Show Less"))
+
+    expect(screen.getByText(truncated)).toBeInTheDocument();
+    expect(screen.getByText("Show More")).toHaveTextContent("Show More");
   });
 });
